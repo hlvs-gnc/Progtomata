3ecc10590fb11c634abbe1d3af2744cf00746871
hlvs-gnc <alvesrique02@gmail.com>
2025-07-22 01:41:39 +0100

    Solve CMake build procedure: program working correctly on board after flashing

--- cmake/toolchain-arm-none-eabi.cmake
+++ cmake/toolchain-arm-none-eabi.cmake
@@ -6,6 +6,11 @@ set(CMAKE_SYSTEM_NAME       Generic)
 set(CMAKE_SYSTEM_PROCESSOR  cortex-m4)
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
+set(COMPILER_PREFIX arm-none-eabi-)
+set(CMAKE_C_COMPILER   ${COMPILER_PREFIX}gcc)
+set(CMAKE_ASM_COMPILER ${COMPILER_PREFIX}gcc)
+set(CMAKE_CXX_COMPILER ${COMPILER_PREFIX}g++)
+
 if(DEFINED ENV{GNUARMEMB_TOOLCHAIN})
   set(_TOOLROOT $ENV{GNUARMEMB_TOOLCHAIN})
 elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
@@ -23,5 +28,8 @@ set(CMAKE_OBJCOPY ${ARM_objcopy})
 set(CMAKE_SIZE    ${ARM_size})
 
 set(ARCH_FLAGS -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard)
-add_compile_options(${ARCH_FLAGS} -ffunction-sections -fdata-sections)
-add_link_options   (${ARCH_FLAGS} -Wl,--gc-sections -specs=nosys.specs)
+set(CMAKE_EXE_LINKER_FLAGS "${ARCH_FLAGS} -nostartfiles -Wl,--gc-sections -T${CMAKE_SOURCE_DIR}/stm32f407vg.ld")
+set(CMAKE_ASM_FLAGS "${CPU_FLAGS} -x assembler-with-cpp")
+
+add_compile_options(${ARCH_FLAGS} -Og -g3 -ffunction-sections -fdata-sections)
+add_link_options   (${ARCH_FLAGS} -specs=nosys.specs)
